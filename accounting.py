bf0c43636201ebcabbefb5a3159b314570b48500
George Simpson <george@hackbrightacademy.com>
2020-07-25 23:11:27 +0000

    initial commit: pulled out melon_count function

---
+++ accounting.py
@@ -0,0 +1,53 @@
+DORKY_LINE_LENGTH = 80
+
+print("*" * DORKY_LINE_LENGTH)
+
+
+def melon_count(order_file_name):
+    melon_tallies = {"Musk":0, "Hybrid":0, "Watermelon":0, "Winter": 0}
+    with open(order_file_name) as f:
+        for line in f:
+            (_, melon_type, melon_count) = line.rstrip().split("|")
+
+            melon_tallies[melon_type] += int(melon_count)
+
+    return melon_tallies #this is a dict
+
+melon_tallies = melon_count('orders-by-type.txt')
+
+
+melon_prices = { "Musk":       1.15, 
+                 "Hybrid":     1.30, 
+                 "Watermelon": 1.75, 
+                 "Winter":     4.00 }
+
+total_revenue = 0
+for melon_type in melon_tallies:
+    price = melon_prices[melon_type]
+    revenue = price * melon_tallies[melon_type]
+    total_revenue += revenue
+    # print("We sold %d %s melons at %0.2f each for a total of %0.2f" % (melon_tallies[melon_type], melon_type, price, revenue))
+    print("We sold {} {} melons at {:.2f} each for a total of {:.2f}"
+            .format(melon_tallies[melon_type], melon_type, price, revenue))
+
+
+print("******************************************")
+f = open("orders-with-sales.txt")
+sales = {'internet':0, 'salesperson':0}
+for line in f:
+    d = line.split("|")
+    if d[1] == "0":
+        sales['internet'] += float(d[3])
+    else:
+        sales['salesperson'] += float(d[3])
+
+
+print("Salespeople generated ${:.2f} in revenue.".format(sales['salesperson']))
+print("Internet sales generated ${:.2f} in revenue.".format(sales['internet']))
+
+if sales['salesperson'] > sales['internet']:
+    print("Guess there's some value to those salespeople after all.")
+else:
+    print("Time to fire the sales team! Online sales rule all!")
+
+print("******************************************")
